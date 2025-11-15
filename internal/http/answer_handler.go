8f7e5f73d3b50d2d19145d8ec3d971ff4b60dc2e
maxim m <maxim.marinin@enkod.io>
2025-11-15 22:59:19 +0300

    refactoring: wrap error

--- internal/http/answer_handler.go
+++ internal/http/answer_handler.go
@@ -54,7 +54,7 @@ func (h *AnswerHandler) HandleCreateForQuestion(w http.ResponseWriter, r *http.R
 
 	ans, err := h.svc.CreateAnswer(r.Context(), id, req.UserID, req.Text)
 	if err != nil {
-		if err == service.ErrQuestionNotFound {
+		if errors.Is(err, service.ErrAnswerNotFound) {
 			transport.WriteError(w, http.StatusNotFound, "question not found")
 			return
 		}
